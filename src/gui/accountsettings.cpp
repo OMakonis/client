dc04909501f8aa6f6853b0f23b1e6b1a40d7b6e5
EmilsL <emils.lipsans@gmail.com>
2022-07-28 15:49:21 +0300

    margin left

--- src/gui/accountsettings.cpp
+++ src/gui/accountsettings.cpp
@@ -209,7 +209,7 @@ void AccountSettings::createAccountToolbox()
     ui->_accountToolbox->setText(tr("Account") + QLatin1Char(' '));
     ui->_accountToolbox->setMenu(menu);
     ui->_accountToolbox->setPopupMode(QToolButton::InstantPopup);
-    ui->_accountToolbox->setStyleSheet("QToolButton::menu-indicator { margin: 1em; }");
+    ui->_accountToolbox->setStyleSheet("QToolButton::menu-indicator { margin-left: 1em; }");
 
 }
 
