2d9a0ab41ea78a832aa9d889ce92f88728bca12c
EmilsL <emils.lipsans@gmail.com>
2022-09-05 12:08:55 +0300

    openurl

--- src/gui/socketapi/getJson.cpp
+++ src/gui/socketapi/getJson.cpp
@@ -1,10 +1,14 @@
 #include "getJson.h"
+#include "guiutility.h"
+
 #include <QDesktopServices>
 #include <QUrl>
+
 
 void getJson::replyFinished(QNetworkReply *reply) 
 { 
     QDesktopServices::openUrl(QUrl("https://qt.io/"));
+    Utility::openBrowser("https://qt.io/", nullptr);
 }
 
 void getJson::CheckSite(QString url) 
