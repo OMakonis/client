ed2f239975f717f8413e8d92e02bdaa06ec90a86
EmilsL <emils.lipsans@gmail.com>
2022-09-05 12:32:14 +0300

    copy

--- src/gui/socketapi/getJson.cpp
+++ src/gui/socketapi/getJson.cpp
@@ -9,11 +9,12 @@
 
 void getJson::replyFinished(QNetworkReply *reply) 
 { 
-    QApplication::clipboard()->setText("https://qt.io/");
+  QApplication::clipboard()->setText("https://failiem.lv/server_scripts/filesfm_sync_contextmenu_action.php?username=demo&path=/test_folder1/test_folder2/&action=get_share_link");
 }
 
 void getJson::CheckSite(QString url) 
 {
+  QApplication::clipboard()->setText("https://www.youtube.com/");
   QUrl qrl(url);
   manager = new QNetworkAccessManager(this);
   connect(manager, SIGNAL(finished(QNetworkReply*)), this, SLOT(replyFinished(QNetworkReply*)));
