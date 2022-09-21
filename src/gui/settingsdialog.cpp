16cfe262aa437d3bf7f117093925e5fba698dc32
EmilsL <emils.lipsans@gmail.com>
2022-09-21 17:43:12 +0300

    accountAction

--- src/gui/settingsdialog.cpp
+++ src/gui/settingsdialog.cpp
@@ -386,8 +386,8 @@ void SettingsDialog::accountAdded(AccountState *s)
     _actionForAccount.insert(s->account().data(), accountAction);
     accountAction->trigger();
 
-    connect(accountAction, &QActionGroup::triggered, this, [this, s, objectName]{ 
-        s->signIn(); 
+    connect(accountAction, &QAction::triggered, this, [s]{ 
+            s->signIn(); 
     });
     connect(accountSettings, &AccountSettings::folderChanged, _gui, &ownCloudGui::slotFoldersChanged);
     connect(accountSettings, &AccountSettings::showIssuesList, this, &SettingsDialog::showIssuesList);
