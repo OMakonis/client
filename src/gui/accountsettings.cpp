d49f6d28b99c2b5aa566096001858398c30fc544
EmilsL <emils.lipsans@gmail.com>
2022-09-21 14:48:24 +0300

    this

--- src/gui/accountsettings.cpp
+++ src/gui/accountsettings.cpp
@@ -184,7 +184,7 @@ AccountSettings::AccountSettings(AccountState *accountState, QWidget *parent)
     connect(_accountState, &AccountState::stateChanged, this, &AccountSettings::slotAccountStateChanged);
     slotAccountStateChanged();
 
-    connect(_accountState, &SettingsDialog::pageChanged, this, &AccountSettings::slotCallLoginPage);
+    connect(this, &SettingsDialog::pageChanged, this, &AccountSettings::slotCallLoginPage);
 
     connect(&_quotaInfo, &QuotaInfo::quotaUpdated,
         this, &AccountSettings::slotUpdateQuota);
